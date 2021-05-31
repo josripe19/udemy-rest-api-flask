b056c50d3895012e50382e1010baf07e3063d30d
Jose Gabriel <josripe19@gmail.com>
2021-05-30 22:45:00 -0400

    initial SQLAlchemy configuration

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ from flask_jwt import JWT
 from resources.user import UserRegister
 from security import authenticate, identity
 from resources.item import Items, Item
+from db import db
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True  # To allow flask propagating exception even if debug is set to false on app
 app.secret_key = 'secret'
 api = Api(app)
@@ -20,4 +23,5 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Items, '/items')
 api.add_resource(UserRegister, '/register')
 
+db.init_app(app)
 app.run()
