c857a1a4dd8d4efc4bc33ba8527dc4ecf7256103
Jose Gabriel <josripe19@gmail.com>
2021-05-30 23:24:01 -0400

    UserModel updated to use SQLAlchemy queries

--- models/user.py
+++ models/user.py
@@ -1,4 +1,3 @@
-import sqlite3
 from db import db
 
 
@@ -9,31 +8,21 @@ class UserModel(db.Model):
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
 
-    def __init__(self, _id, username, password):
-        self.id = _id
+    def __init__(self, username, password):
         self.username = username
         self.password = password
 
     def __str__(self):
         return f"<User id: {self.id}, username: {self.username}>"
 
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
     @classmethod
     def find_by_username(cls, username):
-        return cls.find_by_field(username, 'username')
+        return cls.query.filter_by(username=username).first()
 
     @classmethod
     def find_by_id(cls, _id):
-        return cls.find_by_field(_id, 'id')
-
-    @classmethod
-    def find_by_field(cls, field, field_name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = f"SELECT * FROM users WHERE {field_name} = ?"
-        result = cursor.execute(query, (field,))
-        row = result.fetchone()
-        user = cls(*row) if row else None
-
-        connection.close()
-        return user
+        return cls.query.filter_by(id=_id).first()
--- resources/user.py
+++ resources/user.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask_restful import Resource, reqparse
 from models.user import UserModel
 
@@ -14,13 +13,6 @@ class UserRegister(Resource):
         if UserModel.find_by_username(data['username']):
             return {'message': 'The username is already used'}, 400
 
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO users VALUES(NULL, ?, ?)"
-        cursor.execute(query, (data['username'], data['password']))
-
-        connection.commit()
-        connection.close()
+        UserModel(**data).save_to_db()
 
         return {'message': 'User created successfully'}, 201
