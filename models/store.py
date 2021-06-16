ab77c5135c227059a81b8fda8fa7317a8d89f910
Jose Gabriel <josripe19@gmail.com>
2021-06-15 22:37:58 -0400

    unique names constraint

--- models/store.py
+++ models/store.py
@@ -5,7 +5,7 @@ class StoreModel(db.Model):
     __tablename__ = 'stores'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(90))
+    name = db.Column(db.String(90), unique=True)
 
     items = db.relationship('ItemModel', lazy='dynamic')
 
